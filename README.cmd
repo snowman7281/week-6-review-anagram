0e02b6ce4017a873c1824340fa337aa7b7ae542b
nv <nv781@yahoo.com>
2017-09-08 13:47:20 -0700

    add an updated README.md

--- README.cmd
+++ README.cmd
@@ -1,46 +1,46 @@
-# _{Application Name}_
+# _{week-6-review-anagrams}_
 
-#### _{Brief description of application}, {Date of current version}_
+#### _{ruby BDD}, {09/08/2017}_
 
-#### By _**{List of contributors}**_
+#### By _**{Snow Vilay}**_
 
 ## Description
 
-_{This is a detailed description of your application. Its purpose and usage.  Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. }_
+_{this application has methods to perform certain checks for BDD testing}_
 
 ## Setup/Installation Requirements
 
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
-* _easy-to-understand_
-* _format_
+* _click on link to view_
+* _copy/clone project from link below to refactor for personal use_
+* _open file within spec folder to change test for personal use_
+* _open file within lib folder to refactor methods for personal use_
+* _https://github.com/snowman7281/week-6-review-anagram.git_
 
-_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
 
 ## Specs
 
-* _check for anagrams_
-* _check for anagrams with capitals_
-* _check for palindroms_
+* _check if 2 words are anagrams_
+* _check the words for capitals_
+* _check for palindromes_
 * _check for vowels so they are valid words_
-* _format_
+* _check if words are antigrams_
+* _check for spaces, punctuations and special characters_
 
 
 ## Known Bugs
 
-_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+_{no known bugs }_
 
 ## Support and contact details
 
-_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+_{contact the developer: Snow Vilay}_
 
 ## Technologies Used
 
-_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+_{terminal, ruby 2.2.0, atom }_
 
 ### License
 
-*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+*{Licensed under the Mit License.}*
 
-Copyright (c) 2016 **_{List of contributors or company name}_**
+Copyright (c) 2016 **_{Snow Vilay}_**
