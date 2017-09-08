fd2621ad68646015657bd8cf0b81a61bce494306
nv <nv781@yahoo.com>
2017-09-08 11:26:45 -0700

    check for vowels in each method to make it a valid word

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,7 +1,16 @@
 public def is_anagram?(word1, word2)
-  (word1.downcase.chars - word2.downcase.chars).length.zero? ? "These words are anagrams!" : "These words are not anagrams!"
+
+  if(word1.include?("a") || word1.include?("e") || word1.include?("i") || word1.include?("0") || word1.include?("u"))
+    (word1.downcase.chars - word2.downcase.chars).length.zero? ? "These words are anagrams!" : "These words are not anagrams!"
+  else
+    puts "This is not a real word. Enter a word with a vowel to make it valid"
+  end
 end
 
-public def is_palindrome?(word)
-  word = word.reverse
+public def is_palindrome?(word1)
+  if(word1.include?("a") || word1.include?("e") || word1.include?("i") || word1.include?("0") || word1.include?("u"))
+    word1 = word1.reverse
+  else
+    puts "This is not a real word. Enter a word with a vowel to make it valid"
+  end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,10 +3,10 @@ require('rspec')
 
 describe('#anagrams')do
   it("checking for any word")do
-    expect(is_anagram?("bat","tab")).to(eq("These words are anagrams!"))
+    expect(is_anagram?("cat","tac")).to(eq("These words are anagrams!"))
   end
   it("checking for any words that have capitals to equal to anagrams")do
-    expect(is_anagram?("Bat","Tab")).to(eq("These words are anagrams!"))
+    expect(is_anagram?("Cat","Tac")).to(eq("These words are anagrams!"))
   end
   it("checking for palindrome words")do
     expect(is_palindrome?("cat")).to(eq("tac"))
