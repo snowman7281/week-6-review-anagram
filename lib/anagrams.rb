8144610444b011e72a2fd7962aebd83585b86b3c
nv <nv781@yahoo.com>
2017-09-12 21:46:52 -0700

    update methods to run test to properly check for requirements

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,24 +1,41 @@
-def is_anagram?(word1, word2)
-  if(word1.include?("a") || word1.include?("e") || word1.include?("i") || word1.include?("0") || word1.include?("u"))
-    if(word1.downcase.chars.length == word2.downcase.chars.length)
-      return "These words are anagrams!"
-    else
-      return "These words are not anagrams they are antigram!"
-    end
-  else
-    return "This is not a real word. Enter a word with a vowel to make it valid"
+class AnagramsChecker
+
+  attr_accessor(:word1, :word2)
+
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
   end
-end
 
-def spaces_and_punc(word1)
-   word1.delete("/[.,\/#!$%\^&\*;:'{}=\-_`~()]/g").delete("/\s{2,}/g").delete("[0-9]")
-end
+  def is_anagram?
+    if @word1.include?("a") || @word1.include?("e") || @word1.include?("i") || @word1.include?("o") || @word1.include?("u") || @word2.include?("a") || @word2.include?("e") || @word2.include?("i") || @word2.include?("o") || @word2.include?("u")
+      # if(word1.downcase.chars.length == word2.downcase.chars.length)
+      if @word1.downcase.split('').sort.join == @word2.downcase.split('').sort.join
+        return "These words are anagrams!"
+      # elsif @word1.downcase.chars.length == @word2.downcase.chars.length && @word1.downcase.split('').sort.join == @word2.downcase.split('').sort.join
+      else
+        return "These words are not anagrams they are antigram!"
+      end
+    else
+      return "This is not a real word. Enter a word with a vowel to make it valid"
+    end
+  end
+
+  def spaces_and_punc?
+     @word1.delete("/[.,\/#!$%\^&\*;:'{}=\-_`~()]/g").delete("/\s{2,}/g").delete("[0-9]")
+     @word2.delete("/[.,\/#!$%\^&\*;:'{}=\-_`~()]/g").delete("/\s{2,}/g").delete("[0-9]")
+  end
 
 
-def is_palindrome?(word1)
-  if(word1.include?("a") || word1.include?("e") || word1.include?("i") || word1.include?("0") || word1.include?("u"))
-    word1 = word1.reverse
-  else
-    return "This is not a real word. Enter a word with a vowel to make it valid"
+  def is_palindrome?
+    if @word1.include?("a") || @word1.include?("e") || @word1.include?("i") || @word1.include?("o") || @word1.include?("u") || @word2.include?("a") || @word2.include?("e") || @word2.include?("i") || @word2.include?("o") || @word2.include?("u")
+      if @word1 == @word1.reverse && @word2 == @word2.reverse && @word1 == @word2
+        return @word1
+      else
+        return "This is not a palindrome"
+      end
+    else
+      return "This is not a real word. Enter a word with a vowel to make it valid"
+    end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,20 +1,35 @@
 require('anagrams')
 require('rspec')
 
-describe('#anagrams')do
-  it("checking for any word")do
-    expect(is_anagram?("cat","tac")).to(eq("These words are anagrams!"))
+describe(AnagramsChecker)
+
+  describe('#is_anagram?')do
+    it("checking for any word")do
+      test_anagram = AnagramsChecker.new("cat","tac")
+      expect(test_anagram.is_anagram?()).to(eq("These words are anagrams!"))
+    end
+    it("checking for any words that have capitals to equal to anagrams")do
+      test_anagram = AnagramsChecker.new("Cat","Tac")
+      expect(test_anagram.is_anagram?()).to(eq("These words are anagrams!"))
+    end
+    it("checking for any words that are antigrams.")do
+      test_anagram = AnagramsChecker.new("hi","bye")
+      expect(test_anagram.is_anagram?()).to(eq("These words are not anagrams they are antigram!"))
+    end
+
+
+  describe('#is_palindrome?')do
+    it("checking for palindrome words")do
+      test_anagram = AnagramsChecker.new("dad", "dad")
+      expect(test_anagram.is_palindrome?()).to(eq("dad"))
+    end
   end
-  it("checking for any words that have capitals to equal to anagrams")do
-    expect(is_anagram?("Cat","Tac")).to(eq("These words are anagrams!"))
+
+  describe('#(spaces_and_punc?')do
+    it("checking for any words with spaces or puncuations")do
+      test_anagram = AnagramsChecker.new(" cat's/ * & ^% ", " & ^%cat' &^*^%$)*  s/ *    & ^% ")
+      expect(test_anagram.spaces_and_punc?()).to(eq("cats"))
+    end
   end
-  it("checking for palindrome words")do
-    expect(is_palindrome?("cat")).to(eq("tac"))
-  end
-  it("checking for any words that are antigrams.")do
-    expect(is_anagram?("hi","bye")).to(eq("These words are not anagrams they are antigram!"))
-  end
-  it("checking for any words with spaces or puncuations")do
-    expect(spaces_and_punc(" cat's/ * & ^% ")).to(eq("cats"))
-  end
+
 end
